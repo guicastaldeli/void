f272513c74e589389270cbc4f0ce8c7f5bb06cc8
gui castaldeli <guicastaldeli8@gmail.com>
2025-04-25 17:01:59 -0400

    uodate-01

---
+++ app/src/main.js
@@ -0,0 +1,15 @@
+import './styles/styles.css';
+
+import Text from './text.js';
+import Void from './void.js';
+
+export default function Main() {
+    return (
+        <>
+            <p>Main</p>
+
+            <Text />
+            <Void />
+        </>
+    )
+}
---
+++ app/src/void.js
@@ -0,0 +1,7 @@
+export default function Void() {
+    return (
+        <>
+            <p>void</p>
+        </>
+    )
+}
